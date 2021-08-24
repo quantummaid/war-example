82e0fd404ba3ee01a1652e080847ccd350d868c1
QuantumMaid Developer <developer@quantummaid.de>
2021-08-24 13:48:54 +0200

    third  Stage complete, test working

--- src/test/java/de/quantummaid/tutorials/GreetingTest.java
+++ src/test/java/de/quantummaid/tutorials/GreetingTest.java
@@ -1,4 +1,28 @@
 package de.quantummaid.tutorials;
 
-public final class GreetingTest {
+import de.quantummaid.quantummaid.QuantumMaid;
+import de.quantummaid.quantummaid.integrations.junit5.QuantumMaidTest;
+import de.quantummaid.quantummaid.integrations.testsupport.QuantumMaidProvider;
+import org.junit.jupiter.api.Test;
+
+import static de.quantummaid.tutorials.WebService.createQuantumMaid;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+
+@QuantumMaidTest
+public final class GreetingTest implements QuantumMaidProvider {
+
+    @Override
+    public QuantumMaid provide(final int port) {
+        return createQuantumMaid(port);
+    }
+
+    @Test
+    public void testGreeting() {
+        given()
+                .when().get("/hello/quantummaid")
+                .then()
+                .statusCode(200)
+                .body(is("\"hello quantummaid\""));
+    }
 }
